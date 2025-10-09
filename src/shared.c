0ca1dbd978a3b32c77e10be35ab8566279d3bcf1
Ivan Perfilov <lordvan2013@gmail.com>
2025-10-10 02:08:08 +0300

    Added db_insert and db_update functions

--- src/shared.c
+++ src/shared.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "shared.h"
 
 int main() {
@@ -11,6 +13,7 @@ int db_select_all(const char *filename, void* buffer, size_t struct_size) {
         printf("Ошибка: неверные параметры\n");
         return 0;
     }
+
     FILE *file;
     long file_size;
     int count = 0;
@@ -71,6 +74,115 @@ int db_save_all(const char *filename, void *buffer, size_t struct_size, int coun
 }
 
 
-int db_insert(const char *filename, void *new_record, size_t struct_size);
-int db_update(const char *filename, void *update_record, size_t struct_size, int id);
+int db_insert(const char *filename, void *new_record, size_t struct_size) {
+
+    if (filename = NULL || new_record == NULL || struct_size == 0) {
+        printf("Ошибка: неверные параметры\n");
+        return 1;
+    }
+
+    void *buffer = NULL;
+    int count = 0;
+
+    FILE *file = fopen(filename, "rb");
+
+    if (file != NULL) {
+        fseek(file, 0, SEEK_END);
+        long file_size = ftell(file);
+        rewind(file);
+
+        count = file_size / struct_size;
+
+        if (file_size > 0) {
+            buffer = malloc(file_size);
+            if (buffer != NULL) {
+                fread(buffer, struct_size, count, file);
+            }
+        }
+        fclose(file);
+    }
+    
+    void *new_buffer = realloc(buffer, (count + 1) * struct_size);
+    if (new_buffer == NULL) {
+        free(buffer);
+        return 0;
+    }
+
+    memcpy((char*)new_buffer + count * struct_size, new_record, struct_size);
+    count++;
+
+    int result = db_save_all(filename, new_buffer, struct_size, count);
+    free(new_buffer);
+    return result;
+
+}
+
+int db_update(const char *filename, void *update_record, size_t struct_size, int id) {
+
+    if (filename == NULL || update_record == NULL || struct_size == 0) {
+        printf("Ошибка неверные параметры\n");
+        return 1;
+    }
+
+    void *buffer = NULL;
+    int count = 0;
+
+    FILE *file = fopen(filename, "rb");
+
+    if (file != NULL) {
+        fseek(file, 0, SEEK_END);
+        long file_size = ftell(file);
+        rewind(file);
+
+        count = file_size / struct_size;
+
+        if (file_size > 0) {
+            buffer = malloc(file_size);
+            if (buffer != NULL) {
+                fread(buffer, struct_size, count, file);
+            }
+        }
+        fclose(file);
+    }
+
+    if (buffer == NULL || count == 0) {
+        printf("Фалй пустой или не найден\n");
+        return 1;
+    }
+
+    int found_index = -1;
+    for (int i = 0; i < count; i++) {
+        void *current_record = (char*)buffer + i * struct_size;
+
+        int *current_id = (int*)current_record;
+
+        if (*current_id == id) {
+            found_index = i;
+            break;
+        }
+    }
+
+    if (found_index == -1) {
+        printf("Ошибка: запись с ID %d не найдена\n", id);
+        free(buffer);
+        return 1;
+    }
+    void *record_to_update = (char*)buffer + found_index *struct_size;
+    memcpy(record_to_update, update_record, struct_size);
+
+    int result = db_save_all(filename, buffer, struct_size, count);
+
+    if (result) {
+        printf("Запись с ID %d успешно обновлена\n");
+    } else {
+        printf("Ошибка при создании обновлённой записи\n");
+    }
+
+    free(buffer);
+
+    return result;
+    
+}
+
+
 int db_delete(const char *filename, int id, size_t struct_size);
